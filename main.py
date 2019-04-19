c4730081f366feff2baa5485af70eefc5da3a53c
Chris Tran <chris.l.tran.2016@gmail.com>
2019-04-19 11:30:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,20 @@ y[y == 0] = -1
 treatment[treatment == 1] = 0
 treatment[treatment == 2] = 1
 
-np.random.seed(10000)
-
 treatment = np.random.randn(y.shape[0])
 
 x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(x, y, treatment,
                                                                              test_size=0.5, random_state=42)
 
+ct = CausalTree()
+ct.fit(x_train, y_train, treat_train)
+effect_prediction = ct.predict(x_test)
+# print(effect_prediction)
+
+# testing continuous
+np.random.seed(10000)
+x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(x, y, treatment,
+                                                                             test_size=0.5, random_state=42)
 ct = CausalTree(cont=True)
 ct.fit(x_train, y_train, treat_train)
 effect_prediction = ct.predict(x_test)
